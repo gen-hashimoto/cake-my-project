165e477284d1ed8b5b59738641045c868291b90d
g.hashimoto <egghershey@gmail.com>
2025-03-30 00:36:19 +0900

    chap 20 done

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -13,6 +13,12 @@ use App\Controller\AppController;
  */
 class UsersController extends AppController
 {
+    public function initialize()
+    {
+        parent::initialize();
+        $this->Auth->allow(['logout']);
+    }
+
     /**
      * Index method
      *
